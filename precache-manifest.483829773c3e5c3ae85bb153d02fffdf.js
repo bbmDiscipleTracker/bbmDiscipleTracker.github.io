3555b4397fac3029420c55cd53b6961d8c00eeff
Lindany_Retro <mabasolindanircardo@gmail.com>
2020-03-10 17:30:49 +0200

    Update everything

---
+++ precache-manifest.483829773c3e5c3ae85bb153d02fffdf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e540bec8e2a22319d247c75448020458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "355bdfe93a94137436a4",
+    "url": "/static/css/2.bdfbcfa7.chunk.css"
+  },
+  {
+    "revision": "e14426661780ee5fa828",
+    "url": "/static/css/main.a7368306.chunk.css"
+  },
+  {
+    "revision": "355bdfe93a94137436a4",
+    "url": "/static/js/2.d131987b.chunk.js"
+  },
+  {
+    "revision": "af1b71dc8f7cc14f3b4c1f4697374759",
+    "url": "/static/js/2.d131987b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e14426661780ee5fa828",
+    "url": "/static/js/main.85eb478f.chunk.js"
+  },
+  {
+    "revision": "ba67dfceb0ed700039db",
+    "url": "/static/js/runtime-main.99d6d460.js"
+  },
+  {
+    "revision": "32768934a41e13689983094f44c20e43",
+    "url": "/static/media/bmm.32768934.jpeg"
+  },
+  {
+    "revision": "965a7a1a7bd29d1565b5edeca7b4b9b7",
+    "url": "/static/media/icons_mobiscroll.965a7a1a.woff"
+  },
+  {
+    "revision": "c405c13ebacaadfc5def2aa6f9ae7b7d",
+    "url": "/static/media/icons_mobiscroll.c405c13e.ttf"
+  },
+  {
+    "revision": "60a28f32f3aa37a0f19be022fb8bc709",
+    "url": "/static/media/logout.60a28f32.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0f746eb6e871310c956253b4afe6304.js"
+  "/precache-manifest.483829773c3e5c3ae85bb153d02fffdf.js"
 );
 
 self.addEventListener('message', (event) => {
