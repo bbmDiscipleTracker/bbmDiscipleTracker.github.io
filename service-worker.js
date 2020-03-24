eeefc037a7f37d6d07301f4fb5b32f80b46bfac0
Lindany_Retro <mabasolindanircardo@gmail.com>
2020-03-24 08:27:49 +0200

    Update back button

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91c48278edd99950fc5b16b7508e31d9.js"
+  "/precache-manifest.cca8c3a849c5737a409665811ec9ffe0.js"
 );
 
 self.addEventListener('message', (event) => {
