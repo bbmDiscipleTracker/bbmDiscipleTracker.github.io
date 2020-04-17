66bc1cb58f2064a86d1433708c2e0972d9f3d691
Lindany <bbmdiscipletracker@yahoo.com>
2020-04-17 12:11:57 +0200

    update app

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.206b726c3c5ff666a3d796bc64c8f9c2.js"
+  "/precache-manifest.0c414eb0dda85840b84bde1b62d47f18.js"
 );
 
 self.addEventListener('message', (event) => {
